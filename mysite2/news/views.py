abb22204ff6141b28c55c98c721a5a8dbd9daae8
Opex128 <anabelikova9gmail.com>
2024-02-01 19:44:00 +0300

    Редактирование и удаление записей

--- mysite2/news/views.py
+++ mysite2/news/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
-from django.views.generic import DeleteView
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 
 def news_home(requst):
@@ -9,10 +9,22 @@ def news_home(requst):
     return render(requst, "news/news_home.html", {"news": news})
 
 
-class NewsDetailView(DeleteView):
+class NewsDetailView(DetailView):
     model = Articles
     template_name = "news/details_view.html"
     context_object_name = "article"
+
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = "news/create.html"
+    form_class = ArticlesForm
+
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    template_name = "news/news-delete.html"
+    success_url = "/news"
 
 
 def create(requst):
