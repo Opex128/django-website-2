6db387c89a4e3cb9a3d9308088ca4fad69efe19b
Opex128 <anabelikova9gmail.com>
2024-02-01 17:47:31 +0300

    Вывод записей из базы данных

--- mysite2/news/views.py
+++ mysite2/news/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Articles
 
 
 def news_home(requst):
-    return render(requst, "news/news_home.html")
+    news = Articles.objects.order_by("-date")
+    return render(requst, "news/news_home.html", {"news": news})
