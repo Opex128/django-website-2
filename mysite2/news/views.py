1a5432d7252030ba561f513dc65b655666e48e75
Opex128 <anabelikova9gmail.com>
2024-02-01 18:57:51 +0300

    Форма для добавления записей в БД

--- mysite2/news/views.py
+++ mysite2/news/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
+from .forms import ArticlesForm
 
 
 def news_home(requst):
     news = Articles.objects.order_by("-date")
     return render(requst, "news/news_home.html", {"news": news})
+
+
+def create(requst):
+    error = ""
+    if requst.method == "POST":
+        form = ArticlesForm(requst.POST)
+        if form.is_valid:
+            form.save()
+            return redirect("home")
+        else:
+            error = "Форма была неверной"
+
+    form = ArticlesForm()
+    data = {"form": form, "error": error}
+    return render(requst, "news/create.html", data)
