7010662d1ad5cde736e9248a3ce8de4250006a03
Opex128 <anabelikova9gmail.com>
2024-02-02 10:50:42 +0300

    заключительные правки

--- mysite2/main/views.py
+++ mysite2/main/views.py
@@ -6,5 +6,5 @@ def index(request):
     return render(request, "main/index.html", data)
 
 
-def about(request):
-    return render(request, "main/about.html")
+def contacts(request):
+    return render(request, "main/contacts.html")
